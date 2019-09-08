e432f0d5411a49cb4d598afba9071156e4a21f2d
Miracle2000 <54579428+Miracle2000@users.noreply.github.com>
2019-09-08 12:01:07 +0630

    tax Calculator

---
+++ Cal.py
@@ -0,0 +1,5 @@
+tax = 50 / 1000
+price = 10000
+TotalTax = price * tax
+Total = Price + TotalTax
+round(Total)
---
+++ tax.py
@@ -0,0 +1,5 @@
+tax = 50 / 1000
+price = 10000
+TotalTax = price * tax
+Total = price + TotalTax
+print(round(Total))
