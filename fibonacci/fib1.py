35981329f3bfb3ed2bc7e71e2564afb95997310a
Miracle2000 <54579428+Miracle2000@users.noreply.github.com>
2019-09-29 12:26:23 +0630

    fifth commit

---
+++ fibonacci/fib1.py
@@ -0,0 +1,19 @@
+# import fibonacci
+# fibonacci.fib2(1000)
+# f = fibonacci.fib2(500)
+# print(f)
+
+
+# from fibonacci import *
+#fib(500)
+
+ # from fib, fib2 import fibonacci
+ # fibonacci.fib2(500)
+
+from fib import fibonacci as fibonacci
+fibonacci(500) 
+
+
+
+
+
